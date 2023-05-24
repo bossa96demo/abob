9f6b3a2ecdf1a0019e3a035bc63e609f28b9bac4
bossa96demo <ilyaershov09@gmail.com>
2023-05-24 20:40:07 +0300

    prompt added

--- abob.c
+++ abob.c
@@ -3,6 +3,7 @@
 
 void wcenter(WINDOW *win, int row, char *title);
 void open_start();
+void open_prompt();
 
 WINDOW *start, *prompt;
 
@@ -19,6 +20,19 @@ int main(){
 	y = height >> 1;
 	wcenter(stdscr, y, "ABOB");
 	
+	refresh();
+	noecho();
+
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	do{
+		ch = getch();
+		refresh();
+		if(ch == ':') open_prompt();
+
+	}while(true);
+
+
 	//char cmd[256];
 
 	//mvaddstr(height-1, 0, "Command: ");
@@ -45,17 +59,38 @@ void open_start(){
 
 	getmaxyx(start, height, width);
 	y = height >> 1;
-	//printw("%d cols, %d rows", height, width);
 
 	wcenter(start, y-4, "Welcome to abob");
 	wcenter(start, y-3, "ABOB - Another Buggy, Open Browser");
 	wcenter(start, y-2, "");
-	wcenter(start, y-1, "version 0.0.0.3");
+	wcenter(start, y-1, "version 0.0.0.4");
 	wcenter(start, y,   "type :o<enter> to open a website\n");
 
         touchwin(start);
 
 	wrefresh(start);
+	getch();
+        touchwin(stdscr);
+	refresh();
+
+}
+void open_prompt(){
+	int height, width, y;
+	getmaxyx(stdscr, height, width);
+
+	if((prompt = newwin(1, 0, height - 1, 0)) == NULL){
+		addstr("Error while allocating memory");
+		endwin();
+		return;
+	}
+
+	wbkgd(prompt, COLOR_PAIR(1));
+
+	waddstr(prompt, ":");
+
+        touchwin(prompt);
+
+	wrefresh(prompt);
 	getch();
         touchwin(stdscr);
 	refresh();
