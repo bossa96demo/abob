0d51ec1942c8b9b9e1ea75651e9adfaaf8dca4f3
bossa96demo <ilyaershov09@gmail.com>
2023-05-24 14:00:29 +0300

    lol

--- center.c
+++ center.c
@@ -8,10 +8,16 @@ int main(){
 	int height, width;
 	getmaxyx(stdscr, height, width);
 	int y = height >> 1;
+	printw("%d cols, %d rows", height, width);
 	center(y-3, "welcome to abobr\n");
-	center(y-2, "version 0.0.0.1\n");
-	center(y-1, "press :o<enter> to open a website\n");
+	center(y-2, "version 0.0.0.2\n");
+	center(y-1, "type :o<enter> to open a website\n");
 	center(y, "ABOB");
+	
+	char cmd[256];
+
+	mvaddstr(height-1, 0, "Command: ");
+	getnstr(cmd, 255);
 
 	refresh();
 	getch();
