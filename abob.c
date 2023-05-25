ffd111576471dc410c72df64c432d2c12dc13561
bossa96demo <ilyaershov09@gmail.com>
2023-05-25 16:31:44 +0300

    a lot of fixed, comments added and many more

--- abob.c
+++ abob.c
@@ -2,53 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-void wcenter(WINDOW *win, int row, char *title);
-void open_start();
-void open_prompt();
-void open_website();
-void set_local_clipboard(char *str);
-char *get_local_clipboard();
+void wcenter(WINDOW *win, int row, char *title);// to center text in given window 
+void open_start();				// to open start window
+void open_prompt();				// to open prompt
+void open_website();				//
+void set_url(char *str);			// set url to given string
+char *get_url();				// get url
 
 
-WINDOW *start, *prompt, *website;
+WINDOW *start, *prompt, *website;		// create start and prompt windows
 char *url;
-char *local_clipboard;
 
 int main(){
 	int ch;
-	int height, width, y;
 	initscr();
-	noecho();
+	noecho(); // turning off echoing input
 
 	refresh();
-	
-	open_start();
-	
-	getmaxyx(start, height, width);
-	y = height >> 1;
-	wcenter(stdscr, y, "ABOB");
-	
-	refresh();
+	open_start(); // opening welcome page
 
-	do{
+	do{					/* processing input */
 		ch = getch();
+		touchwin(stdscr);
 		refresh();
-		if(ch == ':') open_prompt();
-		else if(ch == 'y'){
+		if(ch == ':') open_prompt();	/* if user enters command mode */
+		else if(ch == 'y'){		/* if user yanks */
 			clear();
-			printw("Your clipboard is %s", get_local_clipboard());
+			printw("Your clipboard is %s", get_url());
 			refresh();
-		}	
-
+		}else{				/* else printing abob */
+			clear();
+			addstr("ABOB");
+			refresh();
+		}
 	}while(true);
 
-	
-	refresh();
-	getch();
-	endwin();
+	endwin();				/* nothing will go here */
 	return 0;
 }
 
+
+
+
+
+void open_website(char *site){
+	noecho();
+	clear();
+	set_url(site);
+	if((website = newwin(0, 0, 0, 0)) == NULL){
+		addstr("Error while allocating memory");
+		endwin();
+		return;
+	}
+
+	refresh();
+        wprintw(website, "You have opened %s", site);
+
+	touchwin(website);
+	wrefresh(website);
+}
 void open_start(){
 	int height, width, y;
 
@@ -62,18 +74,14 @@ void open_start(){
 	getmaxyx(start, height, width);
 	y = height >> 1;
 
-	wcenter(start, y-4, "Welcome to abob");
-	wcenter(start, y-3, "ABOB - Another Buggy, Open Browser");
-	wcenter(start, y-2, "");
-	wcenter(start, y-1, "version 0.0.0.5");
-	wcenter(start, y,   "type :o<enter> to open a website\n");
+	wcenter(start, y-2, "Welcome to abob");
+	wcenter(start, y-1, "ABOB - Another Buggy, Open Browser");
+	wcenter(start, y-0, "");
+	wcenter(start, y+1, "version 0.0.0.5");
+	wcenter(start, y+2, "type :o<enter> to open a website\n");
 
         touchwin(start);
 	wrefresh(start);
-	getch();
-        touchwin(stdscr);
-	refresh();
-
 }
 void open_prompt(){
 	int height, width;
@@ -90,49 +98,48 @@ void open_prompt(){
         touchwin(prompt);
 	wrefresh(prompt);
 	
-	echo();
+	echo();		// add echo so user will be able to see his commands
 	char all[256];
-	wgetnstr(prompt, all, 256);
-	char *content = strchr(all, ' ') + 1;
-	char *cmd = strtok(all, " ");
-	if(strcmp(cmd, "o") == 0){
-		clear();
-		printw("You have opened %s", content);
-		url = content;
-		set_local_clipboard(url);
-		refresh();
-	}else if(strcmp(cmd, "q") == 0){
-		exit(0);
-	}else if(strcmp(cmd, "y") == 0){
-			}
-	touchwin(stdscr);
+	wgetnstr(prompt, all, 256); // input commands from user
+	
 
-	refresh();
-	noecho();
+	// if user doesn't enter anything programm will crash
+	if(strlen(all) == 0) { noecho(); return; } /* check if so, emergency exit */
+	
+	char *content = strchr(all, ' ') + 1;	// get what goes after command
+	char *cmd = strtok(all, " ");		// get command that user inputed
+	
+	if(strcmp(cmd, "o") == 0 && content[0] != '\0'){// open website on command 'o'
+		open_website(content);
+	}else if(strcmp(cmd, "q") == 0){	// on q we quit the programm
+		exit(0);
+	}else if(strcmp(cmd, "help") == 0){	// display start page on help
+		open_start();
+	}
+	
+	noecho();	// turn off echo 
 }
 void wcenter(WINDOW *win, int row, char *title){
-	int len, indent, y, width;
-       	getmaxyx(win, y, width);
+	int len, indent, y, width;		
+       	getmaxyx(win, y, width);		// getting max x and y of given window
 
-	len = strlen(title);
-	indent = width - len;
-	indent /= 2;
-	mvwaddstr(win, row, indent, title);
-	wrefresh(win);
+	len = strlen(title);			// getting length on the given string
+	indent = width - len;			/* counting how much space */
+	indent /= 2;				/* will we need to retreat */
+	mvwaddstr(win, row, indent, title);	// finally print message
+	wrefresh(win);				// and refresh given window
 }
 
-
-
-void set_local_clipboard(char *str) {
-	if (local_clipboard != NULL) {
-		free(local_clipboard);
+void set_url(char *str) { // we set url for later use, such as copying and saving it
+	if (url != NULL) {			//if url is occupied, delete it
+		free(url);
 	}
-	local_clipboard = (char *)malloc(strlen(str) + 1);
-	strcpy(local_clipboard, str);
+	url = (char *)malloc(strlen(str) + 1);	// allocate space for url
+	strcpy(url, str);			// copy given string in url
 }
 
-char *get_local_clipboard() {
-	return local_clipboard;
+char *get_url() { //simply return url
+	return url;
 }
 
 
